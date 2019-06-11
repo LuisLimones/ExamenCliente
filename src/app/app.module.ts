80de8d17a978367db2652dc8cf2fcaf398066648
EquisFuture <xcaritow@gmail.com>
2019-06-10 23:25:01 -0500

    aañadi los comonentes de registro en blanco para cada uno

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RegistroComponent } from './componentes/uusuarios/registro/registro.component';
+import { LoginComponent } from './componentes/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistroComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
